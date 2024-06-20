087cb303a36ab22da95581ce02b2f24ea6b854d9
Eli N Weinstein <eli.n.weinstein@gmail.com>
2024-06-20 10:46:13 -0400

    feat: scripts for full application runs

--- scripts/application/README.md
+++ scripts/application/README.md
@@ -10,3 +10,16 @@ you can install the main package with an additional option:
 ```
 pip install -e '.[covid]'
 ```
+
+## Workflows
+
+The scripts for the models evaluated in the paper are:
+- `cross_validate.sh` the main CAIRE model
+- `cross_validate_attention.sh` the attention CAIRE model
+- `cross_validate_noselect.sh` the uncorrected model
+- `cross_validate_simple.sh` the simplified non-neural CAIRE model
+
+### Paths
+The above scripts were anonymized as follows: 
+- `PACKAGE_PATH` denotes the path to this package (the folder containing causal-tcrs)
+- `SNYDER_PATH` denotes the path to the preprocessed Snyder et al. data
---
+++ scripts/application/cross_validate_simple.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+REPS=3
+SPLITS=8
+OUTPUT=cir-cv/run-$(date +"%Y-%m-%d-%H-%M-%S")
+echo $OUTPUT;
+for j in $(seq 0 $((REPS-1))); do
+  for i in $(seq 0 $((SPLITS-1))); do
+    python PACKAGE_PATH/causal-tcrs/src/CausalReceptors/select_outcome_model.py \
+    SNYDER_PATH/igor-snyder/preprocessed_igor.hdf5 \
+    --outcome=continuous --flip-outcome --select-latent-dim=4 --repertoire-latent-dim=4 \
+    --select-embed-layer-dim=8 --posterior-rank=-1 --select-posterior-rank=-1 \
+    --selection-channels=8 --encoder-channels=8 \
+    --conv-kernel=3 --selection-conv-kernel=3 --encoder-conv-kernel=3 \
+    --no-attention --n-selection-layers=1 --pos-encode --blosum \
+    --encoder-no-attention --linear-cnn --sum-pool \
+    --optimizer=Adam --unit-batch=8 --unit-batch-eval=8 --subunit-batch=16384 --subunit-batch-eval=16384 \
+    --lr=0.01 --epochs=1000000 --anneal=3 --anneal-time --max-time=10 \
+    --monitor-iter=20 --validate-iter=500 --ninit=10 \
+    --data-seed=$j --seed=$((j*SPLITS+i+1)) \
+    --separate-propensity --propensity-update=10 \
+    --cuda --low-dtype=bfloat16 --num-workers=8 \
+    --weight-decay=0.01 --monitor-converge \
+    --intervene-frac=0.1 --eval-bind=mira --uniform-sample-seq --eval-samples=1 --eval-effect-dist \
+    --splits=$SPLITS --split-choice=$i --stratify-cv --log-output=$OUTPUT
+  done
+done
+python PACKAGE_PATH/causal-tcrs/scripts/application/cross_validate.py $OUTPUT
